921ed284eb353495c383a7c24167a1645d5f324a
Lucas Polidori <92409632+Polidori-112@users.noreply.github.com>
2023-02-09 13:14:13 -0500

    Update Phase2BDRAFT.ino

--- Phase2BDRAFT.ino
+++ Phase2BDRAFT.ino
@@ -1,8 +1,8 @@
-#define M1CTL 9
+#define M1CTL 5
 #define M1A 8
 #define M1B 7
 
-#define M2CTL 5
+#define M2CTL 9
 #define M2A 4 
 #define M2B 2
 
@@ -17,11 +17,31 @@ void setup() {
   
 }
 
+//
+
 void loop() {
   // put your main code here, to run repeatedly:
-  forward(255, 1000);
-  forward(125, 1000);
-
+  // forward(125, 1000);
+  // delay(1000);
+  // backward(125, 1000);
+  crank90_R();
+  crank90_R();
+  crank90_R();
+  crank90_R();
+  crank90_R();
+  crank90_R();
+  crank90_R();
+  crank90_R();
+  crank90_R();
+  crank90_R();
+  crank90_R();
+  crank90_R();
+  delay(1000);
+  // forward(75, 1000);
+  // forward(75, 1000);
+  // delay(1000);
+  // backward(75, 1000);
+  // delay(1000);    
 }
 
 //p is out of 255
@@ -29,12 +49,74 @@ void forward(int p, int ms) {
   // Set forward direction
   digitalWrite(M1B, LOW);
   digitalWrite(M1A, HIGH);
-  
+  digitalWrite(M2B, LOW);
+  digitalWrite(M2A, HIGH);
+
   // Turn motor on 
   analogWrite(M1CTL, p);
-  analogWrite(M2CTL, p)
+  analogWrite(M2CTL, p);
   delay(ms);
   analogWrite(M1CTL, 0);
-  analogWrite(M1CTL, 0);
+  analogWrite(M2CTL, 0);
+}
 
+void backward(int p, int ms) {
+  // Set backward direction
+  digitalWrite(M1A, LOW);
+  digitalWrite(M1B, HIGH);
+  digitalWrite(M2A, LOW);
+  digitalWrite(M2B, HIGH);
+
+  // Turn motor on 
+  analogWrite(M1CTL, p);
+  analogWrite(M2CTL, p);
+  delay(ms);
+  analogWrite(M1CTL, 0);
+  analogWrite(M2CTL, 0);
+}
+
+void crank90_R() {
+  pivot_R(71, 1000);
+}
+
+void crank90_L() {
+  pivot_L(71, 1000);
+}
+
+void pivot_R(int p, int ms) {
+   // Set backward direction
+  digitalWrite(M1B, LOW);
+  digitalWrite(M1A, HIGH);
+  digitalWrite(M2A, LOW);
+  digitalWrite(M2B, HIGH);
+
+  analogWrite(M1CTL, p);
+  analogWrite(M2CTL, p);
+  delay(ms);
+  analogWrite(M1CTL, 0);
+  analogWrite(M2CTL, 0);  
+}
+
+void pivot_L(int p, int ms) {
+   // Set backward direction
+  digitalWrite(M1A, LOW);
+  digitalWrite(M1B, HIGH);
+  digitalWrite(M2B, LOW);
+  digitalWrite(M2A, HIGH);
+
+  analogWrite(M1CTL, p);
+  analogWrite(M2CTL, p);
+  delay(ms);
+  analogWrite(M1CTL, 0);
+  analogWrite(M2CTL, 0);  
+}
+
+void stop() {
+  digitalWrite(M1A, LOW);
+  digitalWrite(M1B, LOW);
+  digitalWrite(M2B, LOW);
+  digitalWrite(M2A, LOW);
+
+  digitalWrite(M1CTL, 0);
+  digitalWrite(M2CTL, 0);  
 }
